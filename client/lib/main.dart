a542af043926c35c2730b5de1f453fec9c7909e2
Ahmed El Otmani <otmaniahmed26@gmail.com>
2024-04-10 17:58:59 +0000

    Update main.dart

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:client/src/utils/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 
+// kick start
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
