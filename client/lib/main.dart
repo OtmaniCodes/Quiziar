168ceadefd28501f43731f07b653b27cf67908b9
Ahmed El Otmani <otmaniahmed26@gmail.com>
2024-05-16 22:04:12 +0100

    Update main.dart

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:client/src/app.dart';
 import 'package:client/src/utils/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
-// stores
+// stores and stores
 
 // kick start the app from here
 // should be over with before AI Domination.
