8c3194a82c649c9e7ceb848ece127d3633045873
Ahmed El Otmani <otmaniahmed26@gmail.com>
2024-06-25 22:55:27 +0100

    Update app.py

--- server/app.py
+++ server/app.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, jsonify, request
 from flask_bcrypt import Bcrypt
 import uuid
-# ......... ...
+# ......... ...sssss
+# data
 from datetime import datetime as dt
 from validations.email_validation import EmailValidartor
 from validations.password_validation import PasswordValidartor
